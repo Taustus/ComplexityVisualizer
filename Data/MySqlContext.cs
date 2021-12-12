1752dd7bdad1b52476739e4a6aafe28794e93938
Сергей Бабиков <ofwgkta_tyler_the_creator@mail.ru>
2021-12-12 16:54:02 +0300

    Fix mispelling. Set table name.

--- Data/MySqlContext.cs
+++ Data/MySqlContext.cs
@@ -4,7 +4,7 @@ namespace Data
 {
     public class MySqlContext : DbContext
     {
-        internal virtual DbSet<StatisticsModel>? StatisticsModels { get; private set; }
+        internal virtual DbSet<StatisticsModel>? StatisticsModel { get; private set; }
 
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
     }
